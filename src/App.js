878257373dc7d8bd06defdc92aa475c69a228c3a
Ryan-Sheehan <sheehan.w.ryan@gmail.com>
2023-09-26 21:27:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as THREE from "three";
+import { Suspense, useRef, useState } from "react";
+import { Canvas, createPortal, useFrame, useThree } from "@react-three/fiber";
+import {
+  useGLTF,
+  ScrollControls,
+  Scroll,
+  useScroll,
+  Preload,
+  Image,
+  MeshTransmissionMaterial,
+  useFBO,
+  useProgress,
+  Html,
+} from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 
-function App() {
+function Loader() {
+  const { progress } = useProgress();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Html center>
+      <div style={{ whiteSpace: "nowrap", fontSize: "13px" }}>
+        {Math.floor(progress)}% loaded
+      </div>
+    </Html>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Canvas
+        gl={{ antialias: false }}
+        camera={{ position: [0, 0, 20], fov: 17 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <ScrollControls damping={0.1} maxSpeed={1} pages={3}>
+            <Lens>
+              <Scroll>
+                <Images />
+              </Scroll>
+              <Preload />
+            </Lens>
+          </ScrollControls>
+        </Suspense>
+      </Canvas>
+    </>
+  );
+}
+
+function Lens({ children, damping = 0.5, ...props }) {
+  const { nodes } = useGLTF("/lens-transformed.glb");
+  const buffer = useFBO();
+  const [scene] = useState(() => new THREE.Scene());
+  const mesh = useRef();
+  const data = useScroll();
+  useFrame((state, delta) => {
+    mesh.current.rotation.y = THREE.MathUtils.lerp(
+      mesh.current.rotation.y,
+      data.offset / 3,
+      damping
+    );
+    // Calculate opacity based on scroll position
+    const opacity = 1 - data.offset; // Adjust maxScrollOffset as needed
+
+    // Set the opacity of the material
+    mesh.current.material.opacity = opacity;
+    state.gl.setRenderTarget(buffer);
+    state.gl.setClearColor("#d8d7d7");
+    state.gl.render(scene, state.camera);
+    state.gl.setRenderTarget(null);
+  });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+
+  return (
+    <>
+      {createPortal(children, scene)}
+      <mesh
+        ref={mesh}
+        scale={[0.8, 0.5, 0.5]}
+        position={[0, 0, isTabletOrMobile ? 8 : 15]}
+        rotation={[Math.PI / 3, Math.PI / 4, 0]}
+        geometry={nodes.Cylinder.geometry}
+        {...props}
+      >
+        <MeshTransmissionMaterial
+          buffer={buffer.texture}
+          thickness={2}
+          anisotropy={2}
+          chromaticAberration={0}
+          ior={1.2}
+        />
+      </mesh>
+    </>
+  );
+}
+
+function Images() {
+  const group = useRef();
+  const data = useScroll();
+  const { width, height } = useThree((state) => state.viewport);
+  useFrame(() => {
+    group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+  });
+  return (
+    <group ref={group}>
+      <Image
+        position={[0, 0, 0]}
+        scale={[width / 2, height, 0.3]}
+        url="/flower.jpeg"
+      />
+      <Image
+        position={[0, -6, 0]}
+        scale={[width / 2, height, 0.3]}
+        url="/shed.jpeg"
+      />
+      <Image
+        position={[0, -12, 0]}
+        scale={[width / 2, height, 0.3]}
+        url="/plains.JPG"
+      />
+    </group>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { useMediaQuery } from "react-responsive";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Overlay() {
+  const isMobile = useMediaQuery({ query: "(max-width: 900px)" });
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        pointerEvents: "none",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          fontStyle: "italic",
+          top: 20,
+          left: 40,
+          right: isMobile ? 40 : "auto",
+          fontSize: "16px",
+          textAlign: isMobile ? "center" : "left",
+        }}
+      >
+        <span>the haunting of periwahn, </span>
+        <span style={{ opacity: "20%" }}>october 2023</span>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          fontStyle: "italic",
+          bottom: 20,
+          right: 20,
+          top: 40,
+          left: isMobile ? 20 : "auto",
+          fontSize: "12px",
+          textAlign: isMobile ? "center" : "right",
+          display: "flex",
+          alignItems: "flex-end",
+        }}
+      >
+        <div
+          style={{
+            maxWidth: isMobile ? "100%" : "240px",
+          }}
+        >
+          Come to Margot's great grandparents farm to celebrate halloweekend.
+          Bring a costume (or 2 or 3) and anything you need to sleep (another
+          costume maybe, up to you). We will have over 100 acres to roam. There
+          will be a big fire. Your friends will be there. If you do not have a
+          car, you can take a bus to Dartmouth and someone will pick you up. If
+          you need to coordinate a ride, reach out (: There will be a grill.
+          Bring some of your own food and/or food to share. Others are welcome—
+          just let me know.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Overlay />
   </React.StrictMode>
 );
 
